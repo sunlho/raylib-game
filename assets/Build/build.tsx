bed653a52dbcb2d43853874a76064f4f0e93d328
sunlho <1784516575@qq.com>
2025-07-09 10:25:27 +0800

    refactor: 1

--- assets/Build/build.tsx
+++ assets/Build/build.tsx
@@ -2,74 +2,76 @@
 <tileset version="1.10" tiledversion="1.11.2" name="build" tilewidth="528" tileheight="577" tilecount="15" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="boat1.png" width="388" height="378"/>
+  <image source="boat1.png" width="240" height="224"/>
   <objectgroup draworder="index" id="2">
-   <object id="6" x="66" y="303">
-    <polygon points="-1,-10 47,-48 136,-67 280,-58 307,-31 307,4 273,45 108,44 46,35"/>
+   <object id="6" x="31.6558" y="179.942">
+    <polygon points="-0.655844,-6.55844 30.8247,-31.4805 89.1948,-43.9416 183.636,-38.039 201.344,-20.3312 201.344,2.62338 179.045,29.513 70.8312,28.8571 30.1688,22.9545"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="1">
-  <image source="build1.png" width="370" height="448"/>
+  <image source="build1.png" width="224" height="272"/>
   <objectgroup draworder="index" id="2">
-   <object id="4" x="237" y="203" width="58" height="32"/>
-   <object id="5" x="265" y="234" width="74" height="69"/>
-   <object id="6" x="4" y="301">
-    <polygon points="0,0 47,-41 166,-68 252,-70 298,-8 300,43 284,88 196,131 102,140 40,133 1,89"/>
+   <object id="4" x="150.789" y="128.667" width="23.3868" height="12.9031"/>
+   <object id="5" x="166.241" y="157.845" width="35.2636" height="24.2143"/>
+   <object id="6" x="10" y="182.412">
+    <polygon points="0,0 27.0548,-23.601 95.5553,-39.1431 145.06,-40.2944 171.539,-4.60508 172.69,24.7523 163.48,50.6558 112.824,75.4081 58.7147,80.5888 23.0254,76.5594 0.575634,51.2315"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="2">
-  <image source="build2.png" width="217" height="288"/>
+  <image source="build2.png" width="128" height="176"/>
   <objectgroup draworder="index" id="2">
-   <object id="1" x="14" y="117" width="195" height="146"/>
+   <object id="1" x="8" y="68.0051" width="121.507" height="82.9897"/>
   </objectgroup>
  </tile>
  <tile id="3">
-  <image source="house1.png" width="384" height="331"/>
+  <image source="house1.png" width="224" height="192"/>
   <objectgroup draworder="index" id="4">
-   <object id="4" x="64" y="153" width="260" height="111"/>
-   <object id="7" x="64" y="267" width="225" height="48"/>
-   <object id="8" x="2" y="169" width="60" height="92"/>
-   <object id="11" x="327" y="230" width="52" height="40"/>
+   <object id="12" x="12" y="114" width="29.5" height="35.5"/>
+   <object id="13" x="41" y="107" width="141.5" height="44"/>
+   <object id="14" x="75.5" y="147" width="73" height="31"/>
+   <object id="15" x="43" y="161" width="25.5" height="15"/>
+   <object id="16" x="187" y="142" width="26.5" height="10.5"/>
   </objectgroup>
  </tile>
  <tile id="4">
-  <image source="house2.png" width="415" height="330"/>
+  <image source="house2.png" width="256" height="192"/>
   <objectgroup draworder="index" id="2">
-   <object id="1" x="9" y="194" width="355" height="121"/>
-   <object id="3" x="366" y="219" width="44" height="60"/>
+   <object id="1" x="15" y="115" width="205.372" height="70"/>
+   <object id="3" x="221.529" y="141.463" width="25.4545" height="22.7107"/>
   </objectgroup>
  </tile>
  <tile id="5">
-  <image source="house3.png" width="375" height="424"/>
+  <image source="house3.png" width="224" height="256"/>
   <objectgroup draworder="index" id="2">
-   <object id="1" x="126" y="241" width="175" height="163"/>
-   <object id="3" x="295" y="380" width="74" height="43"/>
-   <object id="4" x="49" y="298" width="82" height="77"/>
-   <object id="6" x="32" y="264" width="28" height="59"/>
+   <object id="1" x="75.2875" y="154" width="102.308" height="90.6262"/>
+   <object id="3" x="204.255" y="241.095" width="15.9284" height="9.6386"/>
+   <object id="4" x="32.2719" y="195.99" width="47.9387" height="29.0156"/>
+   <object id="6" x="20.3333" y="166.113" width="16.3693" height="27.1592"/>
+   <object id="7" x="177.333" y="234.667" width="18" height="8.66667"/>
   </objectgroup>
  </tile>
  <tile id="6">
-  <image source="house4.png" width="347" height="352"/>
+  <image source="house4.png" width="208" height="192"/>
   <objectgroup draworder="index" id="2">
-   <object id="1" x="25" y="131" width="289" height="169"/>
+   <object id="1" x="18.3333" y="99" width="169.667" height="78.3333"/>
   </objectgroup>
  </tile>
  <tile id="7">
-  <image source="house5.png" width="331" height="358"/>
+  <image source="house5.png" width="192" height="208"/>
   <objectgroup draworder="index" id="2">
-   <object id="1" x="21" y="185" width="213" height="159"/>
-   <object id="2" x="229" y="175" width="98" height="156"/>
+   <object id="1" x="10.3333" y="118.333" width="123.667" height="85.6667"/>
+   <object id="2" x="130.333" y="127" width="54" height="66"/>
   </objectgroup>
  </tile>
  <tile id="8">
-  <image source="house6.png" width="346" height="509"/>
+  <image source="house6.png" width="208" height="288"/>
   <objectgroup draworder="index" id="2">
-   <object id="1" x="21" y="273" width="93" height="166"/>
-   <object id="2" x="109" y="304" width="146" height="158"/>
-   <object id="3" x="255" y="344" width="53" height="85"/>
-   <object id="4" x="311" y="419" width="31" height="17"/>
+   <object id="1" x="11.5" y="220" width="55.2583" height="40.633"/>
+   <object id="2" x="64.7874" y="197.419" width="86.7495" height="75.8796"/>
+   <object id="3" x="150.537" y="217.686" width="33.9913" height="34.0049"/>
+   <object id="4" x="185.311" y="249.75" width="18.4194" height="8.10097"/>
   </objectgroup>
  </tile>
  <tile id="9">
@@ -85,34 +87,34 @@
   </objectgroup>
  </tile>
  <tile id="10">
-  <image source="house8.png" width="378" height="577"/>
+  <image source="house8.png" width="224" height="336"/>
   <objectgroup draworder="index" id="3">
-   <object id="3" x="344" y="428" width="27" height="38"/>
-   <object id="4" x="101" y="423" width="221" height="102">
+   <object id="3" x="205.987" y="260.329" width="15.2763" height="10.5"/>
+   <object id="4" x="64.5" y="246" width="125.039" height="58.7105">
     <ellipse/>
    </object>
-   <object id="7" x="187" y="507" width="61" height="42"/>
-   <object id="8" x="173" y="538" width="50" height="37"/>
+   <object id="7" x="113.658" y="303.026" width="34.5132" height="15.7632"/>
+   <object id="8" x="105.737" y="322.566" width="24.7895" height="10.9342"/>
   </objectgroup>
  </tile>
  <tile id="11">
-  <image source="light.png" width="52" height="153"/>
+  <image source="light.png" width="32" height="96"/>
   <objectgroup draworder="index" id="3">
-   <object id="2" x="9" y="131" width="26" height="16"/>
+   <object id="2" x="9.20833" y="88.375" width="11" height="4.75"/>
   </objectgroup>
  </tile>
  <tile id="12">
-  <image source="tree.png" width="285" height="301"/>
+  <image source="tree.png" width="192" height="192"/>
   <objectgroup draworder="index" id="2">
-   <object id="1" x="9" y="214" width="270" height="85">
+   <object id="1" x="28.5" y="145.5" width="136.667" height="45.1667">
     <ellipse/>
    </object>
   </objectgroup>
  </tile>
  <tile id="13">
-  <image source="tree2.png" width="131" height="139"/>
+  <image source="tree2.png" width="96" height="96"/>
   <objectgroup draworder="index" id="2">
-   <object id="1" x="7" y="107" width="121" height="29">
+   <object id="1" x="14.4545" y="74" width="67.7273" height="19.9091">
     <ellipse/>
    </object>
   </objectgroup>
